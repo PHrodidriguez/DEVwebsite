d36eeb9825ae1fd171d973b68098b4e81b3261c6
Pedro Rodriguez <131567856+PHrodidriguez@users.noreply.github.com>
2023-11-20 21:12:32 -0300

    Devwebysite

---
+++ script.js
@@ -0,0 +1,40 @@
+
+const header = document.querySelector(".heade h5").addEventListener("click",()=>{
+    window.scroll({
+        top: -510,
+        left: 100,
+        behavior: "smooth",
+      });
+});
+
+const start = document.querySelector(".start").addEventListener("click", ()=>{
+    window.scroll({
+        top: 500,
+        left: 100,
+        behavior: "smooth",
+      });
+})
+
+
+const services = document.querySelector(".heade1 h5").addEventListener("click", ()=>{
+    window.scroll({
+        top: 500,
+        left: 100,
+        behavior: "smooth",
+      });
+})
+
+const  projects = document.querySelector(".heade2 h5").addEventListener("click", ()=>{
+    window.scroll({
+        top: 1110,
+        left: 100,
+        behavior: "smooth",
+      });
+})
+const  contact = document.querySelector(".heade3 h5").addEventListener("click", ()=>{
+  window.scroll({
+      top: 1880,
+      left: 100,
+      behavior: "smooth",
+    });
+})
